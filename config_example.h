4b1712496451d327159129b9d40f358fb75899a7
Eldar Venes <eldarvenes@gmail.com>
2022-03-15 11:05:15 +0100

    add example config

---
+++ config_example.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WIFI
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define INFLUXDB_URL ""
+#define INFLUXDB_TOKEN ""
+#define INFLUXDB_ORG ""
+#define INFLUXDB_BUCKET ""
+
+// For emailing
+#define SMTP_HOST ""
+#define SMTP_PORT 465
+#define AUTHOR_EMAIL ""
+#define AUTHOR_PASSWORD ""
+#define RECIPIENT_EMAIL ""
+#define RECIPIENT_EMAIL2 ""
+
+#endif
